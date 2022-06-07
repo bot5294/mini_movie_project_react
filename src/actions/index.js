8cafbd8ac059917207fa1336914c636a24e59d92
bot5294 <5294aqib@gmail.com>
2022-06-07 16:07:13 +0530

    movie mini project react

---
+++ src/actions/index.js
@@ -0,0 +1,44 @@
+// {
+//   type:'ADD_MOVIES',
+//   // movies:[m1,m2,m3]
+// }
+// {
+//   type: "INCREASE_COUNT",
+// }
+// {
+//   type: 'DECREASE_COUNT'
+// }
+
+//  action types
+export const ADD_MOVIES = "ADD_MOVIES";
+export const ADD_TO_FAVOURITES = "ADD_TO_FAVOURITES";
+export const REMOVE_FROM_FAVOURITES = "REMOVE_FROM_FAVOURITES";
+export const SET_SHOW_FAVROUITES = "SET_SHOW_FAVOURITES";
+// action creators
+export function addMovies(movies) {
+  return {
+    type: ADD_MOVIES,
+    movies: movies,
+  };
+}
+
+export function addFavourite(movie) {
+  return {
+    type: ADD_TO_FAVOURITES,
+    movie,
+  };
+}
+
+export function removeFromFavourites(movie) {
+  return {
+    type: REMOVE_FROM_FAVOURITES,
+    movie,
+  };
+}
+
+export function setShowFavourites(val) {
+  return {
+    type: SET_SHOW_FAVROUITES,
+    val,
+  };
+}
